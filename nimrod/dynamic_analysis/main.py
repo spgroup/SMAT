4d1fc208b2d65644a5f440bf8c699ee5a2b10ca2
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-08-31 20:36:28 -0300

    feat: implement method for check semantic conflicts

--- nimrod/dynamic_analysis/main.py
+++ nimrod/dynamic_analysis/main.py
@@ -1,7 +1,23 @@
 from typing import List
+from nimrod.dynamic_analysis.criteria.dynamic_analysis_criteria import DynamicAnalysisCriteria
 from nimrod.test_suites_execution.test_case_execution_in_merge_scenario import TestCaseExecutionInMergeScenario
 
 
+class SemanticConflict:
+  def __init__(self, satisfying_criteria: DynamicAnalysisCriteria, detected_in: TestCaseExecutionInMergeScenario) -> None:
+    self._satisfying_criteria = satisfying_criteria
+    self._detected_in = detected_in
+
 class DynamicAnalysis:
+  def __init__(self, semantic_conflict_criterias: List[DynamicAnalysisCriteria]):
+    self._semantic_conflict_criterias = semantic_conflict_criterias
+
   def check_for_semantic_conflicts(self, test_case_executions: List[TestCaseExecutionInMergeScenario]):
-    pass
+    conflicts = []
+
+    for test_case_execution in test_case_executions:
+      for criteria in self._semantic_conflict_criterias:
+        if criteria.is_satisfied_by(test_case_execution):
+          conflicts.append(SemanticConflict(criteria, test_case_execution))
+
+    return conflicts
--- nimrod/smat.py
+++ nimrod/smat.py
@@ -1,4 +1,5 @@
 from typing import List
+from nimrod.dynamic_analysis.main import DynamicAnalysis
 from nimrod.input_parsing.smat_input import SmatInput
 from nimrod.test_suite_generation.main import TestSuiteGeneration
 from nimrod.test_suite_generation.test_suite import TestSuite
@@ -6,14 +7,15 @@ from nimrod.test_suites_execution.main import TestSuitesExecution
 
 
 class SMAT:
-  def __init__(self, test_suite_generation: TestSuiteGeneration, test_suites_execution: TestSuitesExecution) -> None:
+  def __init__(self, test_suite_generation: TestSuiteGeneration, test_suites_execution: TestSuitesExecution, dynamic_analisys: DynamicAnalysis) -> None:
     self._test_suite_generation = test_suite_generation
     self._test_suites_execution = test_suites_execution
+    self._dynamic_analysis = dynamic_analisys
 
   def run_tool_for_semmantic_conflict_detection(self, scenario: SmatInput) -> None:
     test_suites = self._generate_test_suites_for_scenario(scenario)
     executions = self._test_suites_execution.execute_test_suites(test_suites, scenario.scenario_jars)
-    
+    semantic_conflicts = self._dynamic_analysis.check_for_semantic_conflicts(executions)    
   
   def _generate_test_suites_for_scenario(self, scenario: SmatInput) -> List[TestSuite]:
       suites_left = self._test_suite_generation.generate_test_suites(scenario, scenario.scenario_jars.left)
