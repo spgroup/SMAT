600cd9ebde7916638eafe31779b5426860216f69
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-09-22 17:23:37 -0300

    feat: implement filter to only show exercised targets

--- nimrod/reports_generation/semantic_conflicts_output_generator.py
+++ nimrod/reports_generation/semantic_conflicts_output_generator.py
@@ -3,6 +3,8 @@ from nimrod.dynamic_analysis.semantic_conflict import SemanticConflict
 from nimrod.input_parsing.smat_input import SmatInput
 from nimrod.reports_generation.output_generator import OutputGenerator
 from nimrod.test_suites_execution.main import TestSuitesExecution
+from os import path
+from bs4 import BeautifulSoup
 
 
 class SemanticConflictsOutput(TypedDict):
@@ -26,14 +28,14 @@ class SemanticConflictsOutputGenerator(OutputGenerator[List[SemanticConflictsOut
 
         for semantic_conflict in semantic_conflicts:
             # We need to detect which targets from the input were exercised in this conflict.
-            coverage_report_path = self._test_suites_execution.execute_test_suite_with_coverage(
+            coverage_report_root = self._test_suites_execution.execute_test_suite_with_coverage(
                 test_suite=semantic_conflict._detected_in.test_suite,
                 target_jar=scenario.scenario_jars.merge,
                 test_cases=[semantic_conflict._detected_in.name]
             )
 
             exercised_targets = self._extract_exercised_targets_from_coverage_report(
-                coverage_report_path=coverage_report_path,
+                coverage_report_root=coverage_report_root,
                 targets=scenario.targets
             )
 
@@ -60,17 +62,32 @@ class SemanticConflictsOutputGenerator(OutputGenerator[List[SemanticConflictsOut
 
         return report_data
 
-    def _extract_exercised_targets_from_coverage_report(self, coverage_report_path: str, targets: Dict[str, List[str]]):
+    def _extract_exercised_targets_from_coverage_report(self, coverage_report_root: str, targets: Dict[str, List[str]]):
         exercised_targets: Dict[str, List[str]] = dict()
 
         for class_name in targets.keys():
             for method_name in targets[class_name]:
-                if self._was_target_exercised(coverage_report_path, class_name, method_name):
+                if self._was_target_exercised(coverage_report_root, class_name, method_name):
                     exercised_targets[class_name] = exercised_targets.get(
                         class_name, []) + [method_name]
 
         return exercised_targets
 
-    def _was_target_exercised(self, coverage_report_path: str, class_name: str, method_name: str) -> bool:
-        # First we need to find the class coverage
-        return True
+    def _was_target_exercised(self, coverage_report_root: str, fqcn: str, method_signature: str) -> bool:
+        [package_name, class_name] = fqcn.rsplit('.', 1)
+        class_report_path = path.join(
+            coverage_report_root, package_name, f"{class_name}.html")
+
+        method_name = method_signature[:method_signature.index("(") + 1]
+
+        report_file = open(class_report_path)
+        decoded_report = BeautifulSoup(report_file, 'html.parser')
+        method_report_rows = decoded_report.select("#coveragetable > tbody > tr")
+
+        # We itereate in each method row
+        for method_row in method_report_rows:
+            if method_row.get_text().find(method_name) != -1:
+                if method_row.select_one('td:nth-last-child(2)').get_text() == '0':
+                    return True
+
+        return False
